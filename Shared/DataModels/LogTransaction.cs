b58f537a9c29a0cb327c9e1cefb9b2669b39fb6f
kyrlouca@gmail.com <kyrlouca>
2023-11-09 14:58:15 +0200

    aa

---
+++ Shared/DataModels/LogTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataModels;
+
+internal class LogTransaction { 
+		public int TransactionId { get; set; }
+		public int PensionFundId { get; set; }
+		public string ModuleCode { get; set; }
+		public int ApplicableYear { get; set; }
+		public int ApplicableQuarter { get; set; }
+		public string Message { get; set; }
+		public DateTime TimestampCreate { get; set; }
+		public int UserId { get; set; }
+		public string ProgramCode { get; set; }
+		public string ProgramAction { get; set; }
+		public int InstanceId { get; set; }
+		public string MessageType { get; set; }
+		public string FileName { get; set; }
+}
