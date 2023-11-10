c335c405f2890b6777bd1e93c4d18343ef014c00
kyrlouca@gmail.com <kyrlouca>
2023-11-10 15:02:30 +0200

    yes

--- ConsoleApp1/MainApp.cs
+++ ConsoleApp1/MainApp.cs
@@ -1,11 +1,13 @@
 ﻿namespace ConsoleApp1;
 using Dapper;
+using EntityClasses;
 using Microsoft.Data.SqlClient;
 using Serilog;
 using Shared.CommonRoutines;
 using Shared.DataModels;
 using Shared.HostRoutines;
 using Shared.SharedHost;
+using System;
 using System.Xml.Linq;
 
 public class MyMainApp : IMyMainApp
@@ -37,12 +39,22 @@ public class MyMainApp : IMyMainApp
 			_commonRoutines.CreateTransactionLog(0, MessageType.ERROR, message);
 			return 1;
 		}
-
 		_xmlDoc = ParseXmlFile();
 		if(_xmlDoc == null)
 		{			
 			return 1;
 		}
+
+		var fundLei = GetXmlElementFromXbrl(_xmlDoc, "si1899");
+		var fundFromDb = GetDbFundByLei(fundLei);
+		if (fundFromDb == null || fundFromDb.FundId != _parameterData.FundId)
+		{
+			message = $"The license number is incorrect:{fundLei}";			
+			_logger.Error(message);
+			_commonRoutines.CreateTransactionLog(0, MessageType.ERROR, message);
+			return 1;
+		}
+
 		return 0;
 	}
 
@@ -57,6 +69,12 @@ public class MyMainApp : IMyMainApp
 		if (!File.Exists(_parameterData.FileName))
 		{
 			var message = $"File not FOUND : {_parameterData.FileName}";
+			return (false, message);
+		}
+		var fund = GetDbFundById(_parameterData.FundId);
+		if (fund == null)
+		{
+			var message = $"Fund Id not Found : {_parameterData.FundId}";
 			return (false, message);
 		}
 
@@ -82,4 +100,40 @@ public class MyMainApp : IMyMainApp
 			}
 		return xmlDoc;
 	}
+
+	static string GetXmlElementFromXbrl(XDocument xDoc, string xbrlCode)
+	{
+		//XNamespace ns = "http://CalculatorService/";
+		//var html = xml.Descendants(ns + "html").ToList();
+
+		//<s2md_met:si1899 contextRef="c0">LEI/2138006PEHZTJLNAPC69</s2md_met:si1899>  
+		XNamespace metFactNs = "http://eiopa.europa.eu/xbrl/s2md/dict/met";
+		var leiVal = xDoc.Root.Descendants(metFactNs + xbrlCode).FirstOrDefault()?.Value ?? "";
+		return leiVal;
+	}
+
+	private  FundModel? GetDbFundByLei(string lei)
+	{
+		using var connectionLocal = new SqlConnection(_parameterData.SystemConnectionString);
+
+		if (lei == null)
+			return null;
+
+		lei = lei.Replace(@"LEI/", "");//lei = "LEI/2138003JRMGVH8CGUR42"            
+		var sqlFund = "select  fnd.FundId, fnd.FundName, fnd.IsActive, fnd.Lei , fnd.Wave from Fund fnd where fnd.Lei=@Lei";
+		var fund = connectionLocal.QuerySingleOrDefault<FundModel>(sqlFund, new { lei });
+		return fund;
+	}
+
+
+	private FundModel? GetDbFundById(int fundId)
+	{
+		using var connectionLocal = new SqlConnection(_parameterData.SystemConnectionString);
+		
+		var sqlFund = "Select * from fund fnd where fnd.FundId= @FundId";
+		var fund = connectionLocal.QuerySingleOrDefault<FundModel>(sqlFund, new {fundId });
+		return fund;
+	}
+
+
 }
---
+++ Shared/DataModels/FundModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityClasses
+{
+    
+    public class FundModel
+    {
+        public bool IsActive { get; set; }
+        public string Lei { get; set; }
+        public string FundName { get; set; }
+        public int FundId { get; set; }
+        public int Wave { get; set; }
+    }
+}
