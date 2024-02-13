765ba57888449c6d778092871971d34f0f260e49
kyrlouca@gmail.com <kyrlouca>
2024-02-13 10:32:51 +0200

    unit test

---
+++ TestNewValidations/ValidationTermTest.cs
@@ -0,0 +1,14 @@
+namespace TestNewValidations;
+
+public class ValidationTermTest
+{
+    [Fact]
+    public void TestParser()
+    {
+        var x = 3;
+        Assert.Equal(x, 3);
+    }
+
+
+
+}
