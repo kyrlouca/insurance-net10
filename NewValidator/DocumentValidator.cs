b798de7e2c601dfadb023d80b29883aeeb8223fb
kyrlouca@gmail.com <kyrlouca>
2024-05-15 21:49:49 +0300

    ok

--- NewValidator/DocumentValidator.cs
+++ NewValidator/DocumentValidator.cs
@@ -103,7 +103,7 @@ public class DocumentValidator : IDocumentValidator
         validationRules = validationRules.Where(vr => !exempted.Contains(vr.ValidationID)).OrderBy(rl => rl.ValidationID).ToList();
         if (_parameterData.IsDevelop)
         {
-            validationRules = validationRules.Where(vr => vr.ValidationID == 4078).ToList();
+            //validationRules = validationRules.Where(vr => vr.ValidationID == 4078).ToList();
         }
 
         foreach (var validationRule in validationRules)
