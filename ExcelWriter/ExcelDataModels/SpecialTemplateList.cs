af418d84c10617ef4f8fb3d585f4a954c497dc06
kyrlouca@gmail.com <kyrlouca>
2024-05-18 16:04:08 +0300

    cleared a line

--- ExcelWriter/ExcelDataModels/SpecialTemplateList.cs
+++ ExcelWriter/ExcelDataModels/SpecialTemplateList.cs
@@ -108,15 +108,3 @@ public static class MultiDimensionTemplatesNew
 
     };
 }
-public static class MultiDimensionTemplates
-{
-    public static List<string> CurrencyTemplates = new List<string> {
-            "S.02.02.01.02",
-    };
-    public static List<string> CountryTemplates = new List<string> {
-            "S.04.02.01.02",
-            "S.04.04.01.02",
-            "S.04.05.01.02",
-    };
-
-}
