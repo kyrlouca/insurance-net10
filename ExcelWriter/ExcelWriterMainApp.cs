2ee0447c40b1f8e8f0b5eb8b490ab6c1f9c3d75e
kyrlouca@gmail.com <kyrlouca>
2024-06-18 16:23:18 +0300

    xx

--- ExcelWriter/ExcelWriterMainApp.cs
+++ ExcelWriter/ExcelWriterMainApp.cs
@@ -78,7 +78,7 @@ public class WriterMainApp : IExcelWriterMainApp
         var mergedFilename = Path.Combine(dir, $"{fileNoExtension}.xlsx");
 
 
-        if (!_parameterData.IsDevelop && 1 == 1)
+        if (!_parameterData.IsDevelop || 1 == 1)
         {
             Console.WriteLine($"\n Create excel Fil3e : {filledFilename}");
             //****************************************************************************************************
@@ -91,7 +91,7 @@ public class WriterMainApp : IExcelWriterMainApp
                 return 1;
             }
         }
-        if (!_parameterData.IsDevelop && 1 == 1)
+        if (!_parameterData.IsDevelop || 1 == 1)
         {
             Console.WriteLine($"\n Fill Excel : {filledFilename}");
             //****************************************************************************************************
@@ -105,7 +105,7 @@ public class WriterMainApp : IExcelWriterMainApp
             }
 
         }
-        if (!_parameterData.IsDevelop && 1 == 1)
+        if (!_parameterData.IsDevelop || 1 == 1)
         {
             Console.WriteLine($"\n Merge TabSheets  : {mergedFilename}");
             //****************************************************************************************************
